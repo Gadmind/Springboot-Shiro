40b111f0ce53a1d68f0ce60079f76fc53853c07e
Gadmind <daopudu@sina.com>
2020-06-04 17:04:56 +0800

    添加SQL文件

--- sql/Shiro.sql
+++ sql/Shiro.sql
@@ -102,7 +102,7 @@ CREATE TABLE `sys_user_role`  (
 -- ----------------------------
 -- Records of sys_user_role
 -- ----------------------------
-INSERT INTO `sys_user_role` VALUES ('', '1', '1');
+INSERT INTO `sys_user_role` VALUES ('1', '1', '1');
 INSERT INTO `sys_user_role` VALUES ('2', '2', '2');
 INSERT INTO `sys_user_role` VALUES ('3', '2', '1003');
 
